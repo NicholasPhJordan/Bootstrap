7a37958541425af7e67b1bc3b2f309f7bba758ca
NicholasPhJordan <NicholasPhJordan@gmail.com>
2021-10-12 13:10:56 -0500

    Created DragonWorld and began creating Cube

--- GraphicsProject/main.cpp
+++ GraphicsProject/main.cpp
@@ -1,10 +1,12 @@
 #include "Engine.h"
 #include "TestWorld.h"
+#include "DragonWorld.h"
 
 int main() {
-	Engine* engine = new Engine(1280, 720, "Graphics");
+	Engine* engine = new Engine(1280, 720, "DragonWorld");
 
-	TestWorld* world = new TestWorld();
+	//TestWorld* world = new TestWorld();
+	DragonWorld* world = new DragonWorld();
 
 	engine->setActiveWorld(world);
 
